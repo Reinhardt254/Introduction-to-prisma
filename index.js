49e909cf5ce4f25ae193b5c97324c1e9bdb1e4f4
Reinhardt Lagat <renhardtlagat@gmail.com>
2023-07-11 10:27:03 +0300

    s

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express()
+
+app.use(express.json());
+
+app.listen(3001, () => console.log(`server s running on port ${3001}`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
